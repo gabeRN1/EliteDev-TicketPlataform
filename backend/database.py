9dd643775850488115cc78f5ae2bc57957f7f13a
GabeRN1 <gabj111222@gmail.com>
2026-08-13 16:48:17 +0000

    fix: atualiza database.py para usar variaveis postgres da vercel

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,33 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DB_URL = "sqlite:///./app.db"
+DATABASE_URL = (
+    os.getenv("POSTGRES_URL") or 
+    os.getenv("POSTGRES_URL_NON_POOLING") or 
+    os.getenv("DATABASE_URL") or 
+    "sqlite:///./app.db"
+)
 
-engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False})
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(
+        DATABASE_URL, 
+        connect_args={"check_same_thread": False}
+    )
+else:
+
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True,  
+        pool_recycle=300     
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
