fa793c3f2aec9b3777a5cd9151711748ca6f0a36
GabeRN1 <gabj111222@gmail.com>
2026-08-13 17:58:26 +0000

    corrigindo DATABSE_URL com supabase

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -13,6 +14,14 @@ DATABASE_URL = (
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
+if "postgresql" in DATABASE_URL and "?" in DATABASE_URL:
+    parsed_url = urlparse(DATABASE_URL)
+    query_params = parse_qsl(parsed_url.query)
+    
+    filtered_params = [(k, v) for k, v in query_params if k not in ("supa", "pgbouncer")]
+
+    new_query = urlencode(filtered_params)
+    DATABASE_URL = urlunparse(parsed_url._replace(query=new_query))
 
 if "sqlite" in DATABASE_URL:
     engine = create_engine(
@@ -20,7 +29,6 @@ if "sqlite" in DATABASE_URL:
         connect_args={"check_same_thread": False}
     )
 else:
-
     engine = create_engine(
         DATABASE_URL,
         pool_pre_ping=True,  
