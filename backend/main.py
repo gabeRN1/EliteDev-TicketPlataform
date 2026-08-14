1795a9ac4b7caedb5dbcd0de2b4a447c11d89d09
Gabriel dos Reis <165956199+gabeRN1@users.noreply.github.com>
2026-08-14 12:48:26 +0000

    Update main.py para habilitar o CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import eventos_routes, auth_routes
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Plataforma de Eventos")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/, https://elite-dev-ticket-plataform-ypp5.vercel.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_routes.router)
 app.include_router(eventos_routes.router)
